e77f5429d228471415638b892fba26aa26277702
NoniAprillia <165237591+NoniAprillia@users.noreply.github.com>
2024-09-12 07:43:13 +0700

    Update 2.Encapsulation.php

--- Jobsheet1/2.Encapsulation.php
+++ Jobsheet1/2.Encapsulation.php
@@ -6,13 +6,6 @@ class Mahasiswa {
     private $nim;
     private $jurusan;
 
-    // Constructor untuk menginisialisasi atribut saat objek dibuat
-    public function __construct($nama, $nim, $jurusan) {
-        $this->nama = $nama;
-        $this->nim = $nim;
-        $this->jurusan = $jurusan;
-    }
-
     // Method getter untuk mendapatkan nama
     public function getNama() { 
         return $this->nama;
@@ -45,20 +38,15 @@ class Mahasiswa {
 }
 
 // Membuat objek Mahasiswa dengan data awal
-$Mahasiswa1 = new Mahasiswa("Noni Aprillia", "230102040", "Komputer dan Bisnis");
+$Mahasiswa1 = new Mahasiswa();
+
+$Mahasiswa1->setNama("Aprillia");
+$Mahasiswa1->setNim("230104020");
+$Mahasiswa1->setJurusan("KOMBIS");
 
 // Menggunakan metode getter untuk menampilkan informasi mahasiswa
 echo "Nama: " . $Mahasiswa1->getNama() . '<br>';
 echo "NIM: " . $Mahasiswa1->getNim() . '<br>';
 echo "Jurusan: " . $Mahasiswa1->getJurusan() . '<br><br>';
 
-// Menggunakan metode setter untuk mengubah informasi mahasiswa
-$Mahasiswa1->setNama("Aprillia");
-$Mahasiswa1->setNim("230104020");
-$Mahasiswa1->setJurusan("KOMBIS");
-
-// Menampilkan informasi setelah perubahan menggunakan metode getter
-echo $Mahasiswa1->getNama() . '<br>';
-echo $Mahasiswa1->getNim() . '<br>';
-echo $Mahasiswa1->getJurusan() . '<br>';
 ?>
