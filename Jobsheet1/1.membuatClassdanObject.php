155a75059418a31256b7ac5e6b94f24c35f79d3f
NoniAprillia <165237591+NoniAprillia@users.noreply.github.com>
2024-09-12 07:36:50 +0700

    Update 1.membuatClassdanObject.php

--- Jobsheet1/1.membuatClassdanObject.php
+++ Jobsheet1/1.membuatClassdanObject.php
@@ -1,27 +1,21 @@
 <?php
-// Definisi class Mahasiswa
+//DEFINISIKAN CLASS
 class Mahasiswa {
-    // Atribut atau properti dari class Mahasiswa
+    //ATRIBUT
     public $nama;
     public $nim;
     public $jurusan;
 
-    // Constructor untuk menginisialisasi atribut
-    public function __construct($nama, $nim, $jurusan) {
-        $this->nama = $nama;
-        $this->nim = $nim;
-        $this->jurusan = $jurusan; 
-    }
-
-    // Method untuk menampilkan data mahasiswa
-    public function tampilkanData() {
-        return " <br> Nama : $this->nama </br> <br> NIM : $this->nim </br> <br> Jurusan : $this->jurusan.</br>";
+    //METODE
+    public function tampilkanData(){
+        return " <br> Nama : $this->nama </br> <br> NIM : $this->nim </br> <br> Jurusan : $this->jurusan </br>";
     }
 }
-
-// Instansiasi objek Mahasiswa dengan data tertentu
-$Mahasiswa1 = new Mahasiswa("Noni Aprillia", "230102040", "Komputer dan Bisnis");
-
-// Menampilkan data mahasiswa
-echo $Mahasiswa1->tampilkanData();
+    //INSTANSIASI OBJEK
+    $Mahasiswa1 = new Mahasiswa();
+    $Mahasiswa1->nama="Noni Aprillia";
+    $Mahasiswa1->nim="230102040";
+    $Mahasiswa1->jurusan="Komputer dan Bisnis";
+    echo $Mahasiswa1->tampilkanData();
+    
 ?>
